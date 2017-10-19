3c58a4ffeb9a54d1d58b33d4a8913f91a6e911eb
Quang Dinh <quangdinh1709@gmail.com>
2017-10-19 16:01:41 +0700

    Test Jenkins auto build

--- server.js
+++ server.js
@@ -49,29 +49,52 @@ app.get('/sheet',(req, res) => {
 });
 
 app.post('/sheet/fetch', (req, res) => {
-    var body = '';
+    // var body = '';
+    //
+    // req.on('data', function (data) {
+    //   body += data;
+    //   // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+    //   if (body.length > 1e6) {
+    //     // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+    //     req.connection.destroy();
+    //   }
+    // });
+    //
+    // req.on('end', function () {
+    //   POST = querystring.parse(body);
+    //   console.log(POST);
+    //
+    //   var doc = new GoogleSpreadsheet('1B3nZwqs7Xzd7bXlDYfT_c_PBzqwfhFdCmA7GugGR5cc');
+    //   var temp = encodeURIComponent("Core 2 Duo E7400");
+    //   console.log(temp);
+    //   doc.useServiceAccountAuth(creds, function (err) {
+    //       doc.getRows(1,{
+    //         offset: 1,
+    //
+    //         // orderby: 'number',
+    //         query: `computername=EA用社内PC59`
+    //       },function(err,rows) {
+    //         if(err) {
+    //           return res.status(400).send(err);
+    //         }
+    //         // var data = {
+    //         //   'computername' : rows[0].computername,
+    //         //   'username' : rows[0].username,
+    //         //   'password' : rows[0].password
+    //         // };
+    //         // res.send(data);
+    //         var p = parseInt(rows[0].number, 10);
+    //         console.log(typeof p);
+    //         rows[0].number = p + 1;
+    //         rows[0].save(() => {
+    //             res.send(rows);
+    //         });
+    //
+    //       });
+    //     });
+    // });
 
-    req.on('data', function (data) {
-      body += data;
-      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-      if (body.length > 1e6) {
-        // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-        req.connection.destroy();
-      }
-    });
-
-    req.on('end', function () {
-      POST = querystring.parse(body);
-      console.log(POST);
-
-      var doc = new GoogleSpreadsheet(POST.sheetkey);
-      doc.useServiceAccountAuth(creds, function (err) {
-          doc.getRows(POST.sheetid,{
-          },function(err,rows) {
-            res.send(rows);
-          });
-    });
-});
+    res.send('Hello');
 
 });
 
